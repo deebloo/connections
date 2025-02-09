01828b8bc9ef9280ef619cd0bb3d971d05fd3595
deebloo <deebloo@proton.me>
2025-02-08 23:05:11 -0500

    connections animation

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,47 @@
-console.log("TEST")
+const cells = document.querySelector(".connections");
+
+if (!cells) {
+  throw new Error("its broken");
+}
+
+let selected: HTMLButtonElement[] = [];
+
+cells.addEventListener("click", (e) => {
+  if (e.target instanceof HTMLButtonElement) {
+    if (e.target.classList.contains("connections-cell")) {
+      if (selected.length < 4) {
+        e.target.classList.add("active");
+
+        selected.push(e.target);
+      }
+    }
+  }
+});
+
+let insertRef: Element | null = null;
+
+document.addEventListener("click", (e) => {
+  if (e.target instanceof HTMLButtonElement) {
+    if (e.target.id === "guess") {
+      const bgColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+      document.startViewTransition(() => {
+        for (const el of selected) {
+          if (!insertRef) {
+            cells.prepend(el);
+          } else {
+            insertRef.insertAdjacentElement("afterend", el);
+          }
+
+          insertRef = el;
+
+          el.style.backgroundColor = bgColor;
+
+          el.classList.remove("active");
+        }
+
+        selected = [];
+      });
+    }
+  }
+});
